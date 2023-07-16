d7924e53971036e7962932a636ae1f8c193be2ad
Anuj Kumar Singh <113827742+AkRockBoy@users.noreply.github.com>
2023-07-16 15:05:49 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        sm = 0
+        mx = arr[0]
+        for i in arr:
+            sm += i
+            mx = max(mx , sm)
+            if sm < 0:
+                sm = 0
+        return mx
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
